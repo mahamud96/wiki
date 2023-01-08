3a8540094efbc1fc3643e967fa9c5d3c539845b6
Mahamud Ahmed <mahamud1996@outlook.com>
2023-01-08 18:12:24 +0000

    add error page and separate pages for each article/entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,15 +4,26 @@ from markdown2 import Markdown
 from . import util
 
 def md_conversion(title):
-    page = util.get_entry
+    article = util.get_entry(title)
     markdowner = Markdown()
-    if title == None:
+    if article == None:
         return None
     else:
-        return markdowner.convert(title)
+        return markdowner.convert(article)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    html_article = md_conversion(title)
+    if html_article == None:
+        return render(request, "encyclopedia/errorpage.html", {
+            "error_message": "This article does not exist"
+        })
+    else:
+        return render(request, "encyclopedia/entries.html", {
+            "title": title,
+            "article": html_article,
+        })
