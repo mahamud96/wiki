102446d8eb88117ba04af27f4cecdc72f3706489
Mahamud Ahmed <mahamud1996@outlook.com>
2023-01-08 16:19:41 +0000

    add funtion to convert md files to html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+def md_conversion(title):
+    page = util.get_entry
+    markdowner = Markdown()
+    if title == None:
+        return None
+    else:
+        return markdowner.convert(title)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
